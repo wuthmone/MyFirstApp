266026d32f06953bfca0920618236d4cdce39f1e
wuthmone <whlaing10@gmail.com>
2016-03-26 13:50:43 +0800

    changed some file

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
   get 'greeter/hello' => "greeter#hello"
   get 'greeter/goodby'
 
+  root 'courses#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
